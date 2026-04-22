0318de7e4252488a5db6a9e9ba20c169c99a1a05
Shin2010 <phucha611@gmail.com>
2026-04-22 20:40:23 +0700

    them khung play_game_page.dart

---
+++ lib/pages/play_game_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlayGamePage extends StatelessWidget {
+  const PlayGamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Play Game Page')));
+  }
+}
